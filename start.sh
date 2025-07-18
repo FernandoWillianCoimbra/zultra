85ec56be1f32239e040e4c124b27909b4e6776fd
FERNANDOWILLIANCOIMBRA <fwcoimbra@dinheiroja.com.br>
2025-07-18 18:26:43 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "🔑 Ativando Núcleo Regenerativo Z Ultra..."
+sleep 1
+
+# Criando identificador do nó
+ZID=$(uuidgen)
+echo "🧬 Identificador do nó: $ZID" > zid.log
+
+# Loop de existência viva
+while true; do
+  echo "🟢 [$ZID] Z Ultra executando $(date '+%d/%m/%Y %H:%M:%S')" >> log-zultra.log
+  sleep 30
+done
